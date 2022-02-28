e9f95a066bd28fc513d0fd5fbc7c55bdbec919fd
marikben <70907667+marikben@users.noreply.github.com>
2022-02-28 17:35:48 +0200

    collectionList init, collection function init

--- dbsetup.py
+++ dbsetup.py
@@ -1,52 +1,20 @@
-def get_database():
-    from pymongo import MongoClient
-    import pymongo
+collectionList = [["Tags", "_idQuestionMotif", "textQuestion", "_idTags", "texteReponse"],
+["QuestionParaVitaux", "_idQuestionParaVitaux", "textQuestion", "_idTags", "texteReponse"],
+["QuestionAnamnese", "_idQuestionAnamnese", "textQuestion", "_idTags", "texteReponse"],
+["QuestionMotif", "_idQuestionMotif", "textQuestion", "_idTags", "texteReponse"],
+["Patient", "_idPatient", "description", "genre", "age", "_idSigneVitaux", "_idQuestionParaVitaux",
+ "_idQuestionAnamnese", "_idMotif", "_idQuestionMotif"],
+["Motif", "_idMotif", "description", "_idDegrees", "_idConditions", "_idParamVitaux"],
+["Degree", "_idDegrees", "degree"],
+["Condition", "_idCondition", "_idMotif", "_idDegrees", "textQuestion"],
+["SigneVitaux", "_idSigneVitaux", "glasgow", "_idPupilles", "pulsation", "tas",
+ "tad", "spo2", "peakflowMin", "peakflowCM", "glycemie", "frequenceRespiratoire",
+  "cetonemie", "douleur"], ["Pupilles", "_idPupilles", "pupilleReact", "pupilleSecondaire"]]
 
-    # Provide the mongodb atlas url to connect python to mongodb using pymongo
-    CONNECTION_STRING = "mongodb+srv://maradm:jUQsfK57sxCTzszN@cluster0.npl74.mongodb.net/myFirstDatabase"
-
-    # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
-    #from pymongo import MongoClient
-    client = pymongo.MongoClient(CONNECTION_STRING)
-
-    # Create the database for our example (we will use the same database throughout the tutorial
-    return client['user_shopping_list']
-    
-# This is added so that many files can reuse the function get_database()
+def insertCollections(){
+    #tähän testaat että saatko ekan arvon eli nimen tulostettua
+}
 if __name__ == "__main__":    
-    
-    # Get the database
-    dbname = get_database()
-    collection_name = dbname["user_1_items"]
 
-    item_1 = {
-    "_id" : "U1IT00001",
-    "item_name" : "Blender",
-    "max_discount" : "10%",
-    "batch_number" : "RR450020FRG",
-    "price" : 340,
-    "category" : "kitchen appliance"
-    }
 
-    item_2 = {
-    "_id" : "U1IT00002",
-    "item_name" : "Egg",
-    "category" : "food",
-    "quantity" : 12,
-    "price" : 36,
-    "item_description" : "brown country eggs"
-    }
-
-    collection_name.insert_many([item_1,item_2])
-
-    from dateutil import parser
-    expiry_date = '2021-07-13T00:00:00.000Z'
-    expiry = parser.parse(expiry_date)
-    item_3 = {
-    "item_name" : "Bread",
-    "quantity" : 2,
-    "ingredients" : "all-purpose flour",
-    "expiry_date" : expiry
-    }
-
-    collection_name.insert_one(item_3)
+   print(len(collectionList))
